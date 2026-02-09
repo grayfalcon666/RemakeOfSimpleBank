761a0556c49560f04f0d856416f55b94800a208b
grayfalcon666 <2017215354@qq.com>
2026-02-09 22:09:18 +0800

    fix:use createUserTx instead of createUser and afterFun alone

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -20,13 +20,29 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
 	}
 
-	arg := db.CreateUserParams{
-		Username:       req.GetUsername(),
-		HashedPassword: HashedPassword,
-		FullName:       req.GetFullName(),
-		Email:          req.GetEmail(),
+	arg := db.CreateUserTxParams{
+		CreateUserParams: db.CreateUserParams{
+			Username:       req.GetUsername(),
+			HashedPassword: HashedPassword,
+			FullName:       req.GetFullName(),
+			Email:          req.GetEmail(),
+		},
+		AfterCreate: func(user db.User) error {
+			payload := &worker.PayloadSendVerifyEmail{Username: user.Username}
+			opts := []asynq.Option{
+				asynq.MaxRetry(10),
+				asynq.ProcessIn(10 * time.Second),
+				asynq.Queue("critical"),
+			}
+			err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, payload, opts...)
+			if err != nil {
+				return status.Error(codes.Internal, "failed to distribute task to send verify email")
+			}
+			return nil
+		},
 	}
-	user, err := server.store.CreateUser(ctx, arg)
+
+	result, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
@@ -37,19 +53,8 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
-	payload := &worker.PayloadSendVerifyEmail{Username: user.Username}
-	opts := []asynq.Option{
-		asynq.MaxRetry(10),
-		asynq.ProcessIn(10 * time.Second),
-		asynq.Queue("critical"),
-	}
-	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, payload, opts...)
-	if err != nil {
-		return nil, status.Error(codes.Internal, "failed to distribute task to send verify email")
-	}
-
 	rsp := &pb.CreateUserResponse{
-		User: convertUser(user),
+		User: convertUser(result.User),
 	}
 	return rsp, nil
 }
